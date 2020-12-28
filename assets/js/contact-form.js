dce18d741b7db448a11ceed6b38650bcaf649b67
Sameet Burde <sameet@cyberguardsec.com>
2020-12-28 23:19:14 +0530

    Refactor the default theme layout as per websie requirement, implement form submission

---
+++ assets/js/contact-form.js
@@ -0,0 +1,24 @@
+function resetForm() {
+  $("#contact-form").reset();
+}
+
+function resetMessages() {
+  $("#contact-success").removeClass("success");
+  $("#contact-failure").removeClass("error");
+}
+
+$(document).ready(function () {
+  $("#contact-form").on("submit", function( event) {
+    event.preventDefault();
+
+    $.post(this.action, $(this).serialize(), function(formData) {
+      resetForm();
+      resetMessages();
+      $("#contact-success").addClass("success");
+    })
+    .fail(function(errorData) {
+      resetMessages();
+      $("#contact-failure").addClass("error");
+    })
+  });
+});
