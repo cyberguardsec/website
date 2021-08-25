2109861f2a2a99ae2b1065950085079471575fd4
Sameet Burde <sameet@cyberguardsec.com>
2021-08-25 20:26:05 +0530

    Remove unnecessarily generated files

--- public/js/custom-script.min.7d7f45239113b1f32e6b96d2c450e66bff34696f5bd88a39a11acc0d7662097df0853fb8f27f90a19a59f2c64aa126da.js
+++
@@ -1,5 +0,0 @@
-function resetForm(){$("#contact-form").trigger("reset");}
-function resetMessages(){$("#contact-success").removeClass("success");$("#contact-failure").removeClass("error");}
-function updateNavbar(){var navbar_section=$(".navigation");var navbar_brand=$(".navbar > .navbar-brand");if($(window).scrollTop()<100){navbar_section.addClass("scroll-top");navbar_brand.css("opacity","0");}else{navbar_section.removeClass("scroll-top");navbar_brand.css("opacity","1");}}
-$(document).ready(function(){if($(".hero-area").length>0){updateNavbar();$(window).scroll(function(){updateNavbar();});}
-$("#contact-form").on("submit",function(event){event.preventDefault();$.post(this.action,$(this).serialize(),function(formData){resetForm();resetMessages();$("#contact-success").addClass("success");},"json").fail(function(errorData){resetMessages();$("#contact-failure").addClass("error");})});});
